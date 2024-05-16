c3a552a36c8d5c3d7a1c82b9195c1ee8149c97cb
rafael-batista <rafael.batista@live.com>
2024-05-16 14:43:00 -0300

    Desafio de Controle de Fluxo

---
+++ src/Contador.java
@@ -0,0 +1,32 @@
+package src;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+        int paramUm = terminal.nextInt();
+        int paramDois = terminal.nextInt();
+
+        terminal.close();
+
+        try {
+            contagem(paramUm, paramDois);
+        } catch (ParametrosInvalidosException e){
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    public static void contagem(int paramUm, int paramDois) throws ParametrosInvalidosException{
+        if(paramUm > paramDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = paramDois - paramUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println(i);
+        }
+    }
+}
